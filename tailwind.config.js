aadec328d21c6bf5c6777cec75e74edacb0cdabd
wukdddang <dnr8874@naver.com>
2023-12-12 18:25:59 +0900

    Chore: prettier 설치 + 이미지 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "tw-",
+  prefix: 'tw-',
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        SUCCESS: '#00CC99',
+        ERROR: 'EB5757',
+        WARN: 'F2C94C',
+        INFO: '#5458F7',
+        TEXT: '#252C58',
+        ICON: 'A3AED0',
+      },
+    },
   },
   plugins: [],
 };
