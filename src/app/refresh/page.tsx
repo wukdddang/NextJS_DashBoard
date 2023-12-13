664ee92343d8837985232579618dbf3c58408061
wukdddang <dnr8874@naver.com>
2023-12-13 11:31:39 +0900

    Feat: 리프레시 버튼 누르면 토스트 띄우는 기능 추가

---
+++ src/app/refresh/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { nanoid } from 'nanoid';
+import { redirect } from 'next/navigation';
+
+export default function page() {
+  const toastId = nanoid();
+
+  useEffect(() => {
+    toast('새로고침 했습니다!', {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      toastId: toastId,
+      pauseOnFocusLoss: false,
+    });
+
+    redirect('/');
+  }, []);
+
+  return <></>;
+}
