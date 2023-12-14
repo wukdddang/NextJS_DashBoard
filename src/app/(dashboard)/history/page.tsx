19b3e97c29b1c24f54fb49539cdf76354f3019fd
wukdddang <dnr8874@naver.com>
2023-12-14 17:45:59 +0900

    Refactor: history 폴더 위치 수정

--- src/app/(dashboard)/history/page.tsx
+++ src/app/(dashboard)/history/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import Breadcrumb from '../components/organisms/Breadcrumb';
+import Breadcrumb from '../../components/organisms/Breadcrumb';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
