695fe1831e46b619b8193f9b3a4fcf9ba36657ee
wukdddang <dnr8874@naver.com>
2023-12-19 17:56:57 +0900

    Feat: GlobalStore 상태 추가 - 현재 process의 상태를 나타내는 ProcessState 인터페이스 추가 - TileLayer 상태 저장 코드 추가

--- src/app/store/GlobalStore.ts
+++ src/app/store/GlobalStore.ts
@@ -1,5 +1,7 @@
 import * as L from 'leaflet';
 import { create } from 'zustand';
+import { KIND_OF_MAP_TILES } from '@/app/constants/MapTiles';
+import { processStatusObj } from '@/app/components/organisms/CurrentProcessInfo';
 
 export type EqPointsType = {
   mag: string;
@@ -11,13 +13,19 @@ export type EqPointsType = {
   imageStatus: 'Collecting' | 'Stopped' | 'Collecting1' | 'Paused' | 'Waiting';
 };
 
-interface GlobalState {
+interface ProcessState {
+  processStatus: keyof typeof processStatusObj;
+}
+
+interface GlobalState extends ProcessState {
   currentMap: L.Map | null;
   setCurrentMap: (currentMap: L.Map | null) => void;
   currentEqPoints: EqPointsType[];
   setCurrentEqPoints: (points: EqPointsType[]) => void;
   readEqPoints: (EqPointsType & { is_read: boolean })[];
   setReadEqPoints: (points: (EqPointsType & { is_read: boolean })[]) => void;
+  currentTileLayer: KIND_OF_MAP_TILES;
+  setCurrentTileLayer: (currentTileLayer: KIND_OF_MAP_TILES) => void;
 }
 
 const useGlobalStore = create<GlobalState>((set) => ({
@@ -27,6 +35,9 @@ const useGlobalStore = create<GlobalState>((set) => ({
   setCurrentEqPoints: (points) => set(() => ({ currentEqPoints: points })),
   readEqPoints: [],
   setReadEqPoints: (points) => set(() => ({ readEqPoints: points })),
+  currentTileLayer: 'google_satellite',
+  setCurrentTileLayer: (currentTileLayer) => set(() => ({ currentTileLayer: currentTileLayer })),
+  processStatus: 'Active',
 }));
 
 export default useGlobalStore;
