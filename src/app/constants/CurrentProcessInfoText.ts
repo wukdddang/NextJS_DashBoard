e42fc00f90f6467aec1b46410df481a1ffab2f10
wukdddang <dnr8874@naver.com>
2023-12-19 17:41:00 +0900

    Feat: ProcessText 상수 파일 추가

---
+++ src/app/constants/CurrentProcessInfoText.ts
@@ -0,0 +1,5 @@
+export const currentProcessInfoText = {
+  Active: 'Images are currently being processed!',
+  Completed: 'Images have been processed!',
+  Failed: 'Images have Failed to process!',
+};
