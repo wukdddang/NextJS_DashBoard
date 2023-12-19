9eb55cd1b31568a81422a1b9100b8e59d2946d52
wukdddang <dnr8874@naver.com>
2023-12-19 17:53:31 +0900

    Refactor: CurrentProcessInfo 컴포넌트에 현재 processStatus props 추가

--- src/app/components/organisms/CurrentProcessInfo.tsx
+++ src/app/components/organisms/CurrentProcessInfo.tsx
@@ -2,8 +2,23 @@
 
 import getElapsedTime from '@/app/utils/getProcessingTime';
 import { useEffect, useState } from 'react';
+import StatusIcons from '@/app/utils/StatusIcons';
+import { currentProcessInfoText } from '@/app/constants/CurrentProcessInfoText';
 
-export default function CurrentProcessInfo({ startTime }: { startTime: Date }) {
+export const processStatusObj = {
+  // Delayed: 'WARN',
+  // Waiting: 'ICON',
+  Active: 'INFO',
+  Completed: 'SUCCESS',
+  Failed: 'ERROR',
+};
+
+type Props = {
+  startTime: Date;
+  processStatus: keyof typeof processStatusObj;
+};
+
+export default function CurrentProcessInfo({ startTime, processStatus }: Props) {
   const [currentTime, setCurrentTime] = useState<Date>();
 
   useEffect(() => {
@@ -14,40 +29,27 @@ export default function CurrentProcessInfo({ startTime }: { startTime: Date }) {
     return () => clearInterval(timer);
   }, []);
 
+  const statusStyle = `tw-rounded-full tw-bg-${processStatusObj[processStatus]}`;
+
   return (
-    <div className="tw-hover:bg-indigo-400 tw-inline-flex tw-w-[450px] tw-items-center tw-gap-4 tw-rounded-md tw-bg-INFO tw-bg-opacity-10 tw-px-4 tw-py-2 tw-font-semibold tw-leading-5 tw-text-white tw-shadow tw-transition tw-duration-150 tw-ease-in-out">
-      <div className="tw-rounded-full tw-bg-INFO">
-        <svg
-          className="tw-flex tw-h-[50px] tw-w-[50px] tw-animate-spin tw-items-center tw-justify-center tw-p-2 tw-text-white tw-opacity-100"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="tw-opacity-25"
-            cx="10"
-            cy="10"
-            r="0"
-            stroke="currentColor"
-            strokeWidth="1"
-          ></circle>
-          <path
-            fill="currentColor"
-            strokeWidth="0.5" // Reduced stroke width
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
+    <div
+      className={`tw-inline-flex tw-w-[450px] tw-items-center tw-gap-4 tw-rounded-md ${statusStyle} tw-bg-opacity-10 tw-px-4 tw-py-2 tw-font-semibold tw-leading-5 tw-text-white tw-shadow tw-transition tw-duration-150 tw-ease-in-out`}
+    >
+      <div className={statusStyle}>
+        <StatusIcons processStatus={processStatus} />
       </div>
       <div>
         <span className="tw-text-[18px] tw-tracking-[-1px] tw-text-TEXT">
-          Images are currently being processed!
+          {currentProcessInfoText[processStatus]}
         </span>
         <div className="tw-text-[14px] tw-font-light tw-tracking-[-1px] tw-text-TEXT tw-opacity-50">
           <span className="tw-flex">Daejeon, Korea ({startTime.toISOString()})</span>
-          <span className="tw-flex">
-            Processing Time: &nbsp;
-            <span>{currentTime ? getElapsedTime(startTime, currentTime) : 'calculating...'}</span>
-          </span>
+          {processStatus === 'Active' && (
+            <span className="tw-flex">
+              Processing Time: &nbsp;
+              <span>{currentTime ? getElapsedTime(startTime, currentTime) : 'calculating...'}</span>
+            </span>
+          )}
         </div>
       </div>
     </div>
