3b323909390b88f3c27ea40876488bc10694c075
wukdddang <dnr8874@naver.com>
2023-12-21 18:03:52 +0900

    Feat: generateRippleRing 함수 기능 추가 + 리팩토링 - marker의 event 타겟으로 클릭 이벤트를 관리하도록 수정 - markersRef props를 받도록 해서 marker를 한번만 추가하도록 수정 - marker 아이콘 사이즈 수정 - marker 클릭 시 읽음 처리하는 api 추가

--- src/app/utils/generateRippleRing.ts
+++ src/app/utils/generateRippleRing.ts
@@ -2,12 +2,15 @@ import { EqPointsType } from '@/app/store/GlobalStore';
 import * as L from 'leaflet';
 import { isCurrentMapExist } from '@/app/types/TypePredicate';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
-import { MAP_CENTER, MAP_ZOOM } from '../containers/MapContainer';
+import { MAP_CENTER, MAP_FLY_TO_SPEED, MAP_ZOOM } from '../containers/MapContainer';
+import { MutableRefObject } from 'react';
+import axios from 'axios';
+import { SERVER_HOST } from '@/app/common/constants/environment.const';
 
 const customIcon = L.divIcon({
   className: 'custom-icon',
   html: '<div class="pulse"></div>',
-  iconSize: [100, 100],
+  iconSize: [30, 30],
 }); // 마커 아이콘 설정
 
 // 지진 펄스 크기를 조정하는 함수
@@ -21,27 +24,42 @@ function adjustPulseSize(zoomLevel: number) {
 }
 
 export default function RippleRing({
+  id,
   currentMap,
+  markersRef,
   mag,
   location,
   lat,
   lng,
   createdAt,
   router,
-}: { currentMap: L.Map; router: AppRouterInstance } & EqPointsType) {
+}: {
+  currentMap: L.Map;
+  markersRef: MutableRefObject<{ [key: string]: L.Marker<any> }>;
+  router: AppRouterInstance;
+} & EqPointsType) {
   if (!isCurrentMapExist(currentMap)) {
     return;
   }
 
-  const marker = L.marker([lat, lng], { icon: customIcon }).addTo(currentMap);
+  // console.log(id);
+  // console.log(markersRef.current[id]);
 
-  marker.on('click', () => {
-    currentMap.setView(marker.getLatLng(), 15); // 새로운 줌 레벨
-  });
+  if (!markersRef.current[id]) {
+    const marker = L.marker([lat, lng], { icon: customIcon }).addTo(currentMap);
+    markersRef.current[id] = marker;
 
-  marker.bindPopup(
-    // TODO: Next.js에서 템플릿 문법 적용하는지? (ex. mustache, thymeleaf)
-    `
+    marker.on('click', () => {
+      // currentMap.setView(marker.getLatLng(), 15); // 새로운 줌 레벨
+      if (currentMap.getZoom() > 10) {
+        return;
+      }
+      currentMap.flyTo(marker.getLatLng(), 10, MAP_FLY_TO_SPEED); // 새로운 줌 레벨
+    });
+
+    marker.bindPopup(
+      // TODO: Next.js에서 템플릿 문법 적용하는지? (ex. mustache, thymeleaf)
+      `
       <div class="tw-flex tw-flex-col tw-font-sans tw-p-2 tw-justify-center tw-items-center">
         <div class="tw-flex tw-gap-2">
           <span class="tw-text-[38px]">${mag}</span>
@@ -79,30 +97,43 @@ export default function RippleRing({
       
       
     `
-  );
-
-  marker.on('popupopen', () => {
-    const detailButton = document.querySelector('#popup-view-detail-button');
-    const resetButton = document.querySelector('#popup-reset-button');
-    if (detailButton) {
-      detailButton.addEventListener('click', () => {
-        router.replace(`/?location=${location}&createdAt=${createdAt}&lat=${lat}&lng=${lng}`, {
-          scroll: false,
+    );
+    marker.on('popupopen', async (e) => {
+      await axios
+        .put(`${SERVER_HOST}/usgs/read/${id}`)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      });
-    }
-    if (resetButton) {
-      resetButton.addEventListener('click', () => {
-        currentMap.closePopup();
-        currentMap.setView(MAP_CENTER, MAP_ZOOM);
-      });
-    }
-  });
 
-  currentMap.on('zoomend', () => {
-    const zoomLevel = currentMap.getZoom();
-    adjustPulseSize(zoomLevel);
-  });
+      const markerElement = e.popup.getElement();
+      const detailButton = markerElement?.querySelector('#popup-view-detail-button');
+      const resetButton = markerElement?.querySelector('#popup-reset-button');
+      if (detailButton) {
+        detailButton.addEventListener('click', () => {
+          router.replace(
+            `/?location=${location}&createdAt=${createdAt}&lat=${lat}&lng=${lng}&mag=${mag}`,
+            {
+              scroll: false,
+            }
+          );
+        });
+      }
+      if (resetButton) {
+        resetButton.addEventListener('click', () => {
+          currentMap.closePopup();
+          // currentMap.setView(MAP_CENTER, MAP_ZOOM);
+          currentMap.flyTo(MAP_CENTER, MAP_ZOOM, MAP_FLY_TO_SPEED);
+        });
+      }
+    });
 
+    currentMap.on('zoomend', () => {
+      const zoomLevel = currentMap.getZoom();
+      adjustPulseSize(zoomLevel);
+    });
+  }
   return null;
 }
