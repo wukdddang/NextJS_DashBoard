0b669a4779909f3b43bcf5fdff8609f451c8489a
wukdddang <dnr8874@naver.com>
2023-12-21 18:22:50 +0900

    Feat: / 페이지 tanstack/react-table 테이블 추가 - UsgsList 데이터 받는 기능 추가

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -4,53 +4,114 @@ import { useSearchParams } from 'next/navigation';
 
 import dynamic from 'next/dynamic';
 import useGlobalStore from '@/app/store/GlobalStore';
-import EarthquakesTable from '@/app/components/organisms/EarthquakesTable';
-import EarthquakePointTable from '../components/organisms/EarthquakePointTable';
+import EarthquakePointTable from '@/app/components/organisms/EarthquakePointTable';
+import { useEffect, useState } from 'react';
+import { getUsgsList } from '@/app/_lib/getUsgsList';
+import EarthquakesTableContainer, {
+  EarthquakesTableType,
+} from '@/app/containers/EarthquakesTableContainer';
 const MapContainer = dynamic(() => import('@/app/containers/MapContainer'), { ssr: false });
 
 export default function Page() {
   const searchParams = useSearchParams();
   const currentEqPoints = useGlobalStore((state) => state.currentEqPoints);
+  const setCurrentEqPoints = useGlobalStore((state) => state.setCurrentEqPoints);
+  const historyTableData = useGlobalStore((state) => state.historyTableData);
+  const setHistoryTableData = useGlobalStore((state) => state.setHistoryTableData);
+  const [isReadEarthquakes, setIsReadEarthquakes] = useState<EarthquakesTableType[]>(
+    [] as EarthquakesTableType[]
+  );
 
   const currentEqPoint = currentEqPoints.find((eqPoint) => {
-    if (eqPoint.location === searchParams.get('location')) {
+    if (
+      eqPoint.location === searchParams.get('location') &&
+      eqPoint.createdAt === searchParams.get('createdAt') &&
+      eqPoint.mag === searchParams.get('mag')
+    ) {
       return eqPoint;
     }
   });
 
+  // USGS API 호출
+  useEffect(() => {
+    getUsgsList().then((res) => {
+      if (!res) {
+        return;
+      }
+
+      const newEqPoints = res?.data.map((item) => {
+        return {
+          id: item.id,
+          createdAt: item.properties.createdAt,
+          isRead: item.is_read,
+          lat: item.geometry.coordinates[1],
+          lng: item.geometry.coordinates[0],
+          location: item.properties.place,
+          mag: String(item.properties.mag),
+          status: item.status,
+        };
+      });
+
+      // history earthquakes용 데이터
+      setHistoryTableData(newEqPoints);
+      // unconfirmed earthquakes용 데이터
+      setCurrentEqPoints(newEqPoints.filter((point) => !point.isRead));
+    });
+  }, []);
+
+  useEffect(() => {
+    // TODO: 리팩토링할 때 타입 단언 수정하기
+    const filteredEqPoints: EarthquakesTableType[] = currentEqPoints.map((eqPoint) => {
+      if (!eqPoint.isRead) {
+        return {
+          mag: eqPoint.mag,
+          date: eqPoint.createdAt,
+          location: eqPoint.location,
+          status: eqPoint.status,
+        };
+      }
+    }) as EarthquakesTableType[];
+
+    setIsReadEarthquakes(filteredEqPoints);
+  }, [currentEqPoints]);
+
   return (
-    <>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-6">
-            <MapContainer />
-          </div>
-          <div className="col-lg-6">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="card">
-                  <div className="card-body">
-                    <div className="table-responsive">
-                      {currentEqPoint ? (
-                        <EarthquakePointTable currentEqPoint={currentEqPoint} />
-                      ) : (
-                        <EarthquakesTable tableTitle="Unconfirmed Earthquakes" />
-                      )}
-                    </div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-lg-6">
+          <MapContainer />
+        </div>
+        <div className="col-lg-6">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="card">
+                <div className="card-body">
+                  <div className="table-responsive">
+                    {currentEqPoint ? (
+                      <EarthquakePointTable currentEqPoint={currentEqPoint} />
+                    ) : (
+                      <EarthquakesTableContainer
+                        data={isReadEarthquakes}
+                        tableTitle="Unconfirmed Earthquakes"
+                      />
+                    )}
                   </div>
                 </div>
               </div>
-              <div className="col-lg-12">
-                <div className="card">
-                  <div className="card-body">
-                    <EarthquakesTable tableTitle="Histories of Last Earthquakes" />
-                  </div>
+            </div>
+            <div className="col-lg-12">
+              <div className="card">
+                <div className="card-body">
+                  <EarthquakesTableContainer
+                    data={historyTableData}
+                    tableTitle="Histories of Last Earthquakes"
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
