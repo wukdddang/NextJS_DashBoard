8c23242bdd399bba47563fdf64c202246902cdea
wukdddang <dnr8874@naver.com>
2023-12-21 18:06:08 +0900

    Feat: EarthquakesTable 관련 컴포넌트 추가

--- src/app/components/organisms/EarthquakesTable.tsx
+++ src/app/components/organisms/EarthquakesTable.tsx
@@ -1,91 +1,119 @@
+import { EarthquakesTableType } from '@/app/containers/EarthquakesTableContainer';
+import HistoryFilter from '@/app/history/HistoryFilter';
+import { flexRender } from '@tanstack/react-table';
+import { Table } from '@tanstack/table-core';
+
 type Props = {
+  table: Table<EarthquakesTableType>;
   tableTitle: string;
   isHistoryPage?: boolean;
 };
 
-export default function EarthquakesTable({ tableTitle, isHistoryPage = false }: Props) {
+export default function EarthquakesTable({ table, tableTitle, isHistoryPage = false }: Props) {
   return (
     <div className="table-responsive">
       <div>
         <h4 className="tw-text-[24px] tw-font-bold tw-tracking-[-1px]">{tableTitle} </h4>
       </div>
-      <table className="table mb-0 table-hover align-middle text-nowrap">
+      <div className="h-2" />
+      <table className="table table-striped">
         <thead>
-          <tr>
-            <th rowSpan={2} colSpan={1} className="border-top-0">
-              Magnitude
-            </th>
-            <th rowSpan={2} colSpan={1} className="border-top-0">
-              Date
-            </th>
-            <th rowSpan={2} colSpan={1} className="border-top-0">
-              Location
-            </th>
-            <th rowSpan={2} colSpan={1} className="border-top-0">
-              Processed
-            </th>
-          </tr>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <>
+                        <div
+                          {...{
+                            className: header.column.getCanSort()
+                              ? 'cursor-pointer select-none'
+                              : '',
+                            onClick: header.column.getToggleSortingHandler(),
+                          }}
+                        >
+                          {flexRender(header.column.columnDef.header, header.getContext())}
+                          {{
+                            asc: ' 🔼',
+                            desc: ' 🔽',
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
+                        {header.column.getCanFilter() ? (
+                          <div>
+                            <HistoryFilter column={header.column} table={table} />
+                          </div>
+                        ) : null}
+                      </>
+                    )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className="d-flex align-items-center">
-                <div className="tw-mr-2">
-                  <a
-                    href="#"
-                    className="btn btn-circle d-flex btn-info text-white tw-items-center tw-justify-center"
-                  >
-                    6.5
-                  </a>
-                </div>
-              </div>
-            </td>
-            <td>2024-01-04T01:23:19</td>
-            <td>Kanyakumari, India</td>
-            <td>
-              <div className="d-flex justify-content-between">
-                <span className="badge bg-danger">Failed</span>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className="d-flex align-items-center">
-                <div className="tw-mr-2">
-                  <a href="#" className="btn btn-circle d-flex btn-orange text-white">
-                    6.5
-                  </a>
-                </div>
-              </div>
-            </td>
-            <td>2024-01-01T11:49:32</td>
-            <td>Bardufoss, Norway</td>
-            <td>
-              <div className="d-flex justify-content-between">
-                <span className="badge bg-info">Waiting</span>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div className="d-flex align-items-center">
-                <div className="tw-mr-2">
-                  <a href="#" className="btn btn-circle d-flex btn-orange text-white">
-                    6.5
-                  </a>
-                </div>
-              </div>
-            </td>
-            <td>2024-01-01T11:49:32</td>
-            <td>Bardufoss, Norway</td>
-            <td>
-              <div className="d-flex justify-content-between">
-                <span className="badge bg-info">Waiting</span>
-              </div>
-            </td>
-          </tr>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  let cellStyle = {};
+
+                  // TODO: 리팩토링할 때 타입 단언 수정하기
+                  if (cell.column.id === 'mag' && parseFloat(cell.getValue() as string) >= 5.0) {
+                    cellStyle = { fontWeight: 'bold', color: 'red' };
+                  }
+                  return (
+                    <td key={cell.id} style={cellStyle}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
+      <div className="h-2" />
+      <div className="tw-flex tw-items-center tw-justify-between tw-gap-2">
+        <div>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.setPageIndex(0)}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {'<<'}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {'<'}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {'>'}
+          </button>
+          <button
+            className="border rounded p-1"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            disabled={!table.getCanNextPage()}
+          >
+            {'>>'}
+          </button>
+        </div>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+          </strong>
+        </span>
+      </div>
+      <div>{table.getPrePaginationRowModel().rows.length} Rows</div>
     </div>
   );
 }
---
+++ src/app/containers/EarthquakesTableContainer.tsx
@@ -0,0 +1,77 @@
+import { useMemo, useState } from 'react';
+import { Earthquake } from './HistoryTableContainer';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  getCoreRowModel,
+  getFacetedMinMaxValues,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+} from '@tanstack/table-core';
+import { useReactTable } from '@tanstack/react-table';
+import EarthquakesTable from '../components/organisms/EarthquakesTable';
+import { fuzzyFilter } from '../history/SortAndFilterByFuzzy';
+
+export type EarthquakesTableType = Pick<Earthquake, 'mag' | 'date' | 'location' | 'status'>;
+
+type Props = {
+  data: EarthquakesTableType[];
+  tableTitle: string;
+  isHistoryPage?: boolean;
+};
+
+export default function EarthquakesTableContainer({ data = [], tableTitle, isHistoryPage }: Props) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [globalFilter, setGlobalFilter] = useState('');
+
+  const tableAccessorObj = [
+    { accessorKey: 'mag', header: () => 'Magnitude' },
+    { accessorKey: 'date', header: () => 'Date' },
+    { accessorKey: 'location', header: () => 'Location' },
+    { accessorKey: 'status', header: () => 'Earthquake Downloading Status' },
+  ];
+
+  const columns = useMemo<ColumnDef<EarthquakesTableType, any>[]>(
+    () =>
+      tableAccessorObj.map((obj) => {
+        return {
+          accessorKey: obj.accessorKey,
+          header: () => obj.header(),
+          cell: (info) => info.getValue(),
+          footer: (props) => props.column.id,
+          enableColumnFilter: false,
+        };
+      }),
+    []
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    state: {
+      columnFilters,
+      globalFilter,
+    },
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: fuzzyFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: false,
+  });
+
+  return <EarthquakesTable table={table} tableTitle={tableTitle} isHistoryPage={isHistoryPage} />;
+}
