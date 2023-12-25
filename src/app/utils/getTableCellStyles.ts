5fd7ff5a4f86a0583cdae9da5f64a1fe1acc090b
wukdddang <dnr8874@naver.com>
2023-12-25 23:34:10 +0900

    Styles: 테이블 스타일 설정

---
+++ src/app/utils/getTableCellStyles.ts
@@ -0,0 +1,63 @@
+import { Cell } from "@tanstack/table-core";
+import tailwindConfig from '../../../tailwind.config.js';
+import { UsgsStatusEnum } from "@/app/enum/usgs.status.enum";
+
+type TailwindColors = {
+  PRIMARY: string;
+  SECONDARY: string;
+  BODY_COLOR: string;
+  SUCCESS: string;
+  ERROR: string;
+  WARN: string;
+  INFO: string;
+  TEXT: string;
+  ICON: string;
+};
+
+export const getTableCellStyles = <T>(cell: Cell<T, unknown>) => {
+  const tailwindColors: TailwindColors = tailwindConfig.theme?.extend?.colors as TailwindColors;
+
+  const successColor = tailwindColors?.SUCCESS;
+  const errorColor = tailwindColors?.ERROR;
+  const warnColor = tailwindColors?.WARN;
+  const infoColor = tailwindColors?.INFO;
+  const primaryColor = tailwindColors?.PRIMARY;
+  const secondaryColor = tailwindColors?.SECONDARY;
+  const bodyColor = tailwindColors?.BODY_COLOR;
+  const iconColor = tailwindColors?.ICON;
+  const textColor = tailwindColors?.TEXT;
+
+  let cellStyle = { fontSize: '14px', color: textColor, fontWeight: 'normal' };
+
+  if (
+    cell.column.id === 'mag' &&
+    parseFloat(cell.getValue() as string) >= 5.0
+  ) {
+    cellStyle = { ...cellStyle, fontWeight: 'bold', color: 'red' };
+  }  else if (
+    cell.column.id === 'status') {
+      cellStyle = { ...cellStyle, fontWeight: 'bold' };
+      if (cell.getValue() === UsgsStatusEnum.COMPLETE) {
+        cellStyle = { ...cellStyle, color: successColor };
+      } else if (cell.getValue() === UsgsStatusEnum.STOPPED) {
+        cellStyle = { ...cellStyle, color: errorColor };
+      } else if (cell.getValue() === UsgsStatusEnum.WAITING) {
+        cellStyle = { ...cellStyle, color: iconColor };
+      } else if (cell.getValue() === UsgsStatusEnum.ACTIVE) {
+        cellStyle = { ...cellStyle, color: infoColor };
+      } else if (cell.getValue() === UsgsStatusEnum.FAILED) {
+        cellStyle = { ...cellStyle, color: errorColor };
+      } else if (cell.getValue() === UsgsStatusEnum.PAUSED) {
+        cellStyle = { ...cellStyle, color: warnColor };
+      } else if (cell.getValue() === UsgsStatusEnum.COLLECTING) {
+        cellStyle = { ...cellStyle, color: secondaryColor };
+      } else if (cell.getValue() === UsgsStatusEnum.COLLECTING1) {
+        cellStyle = { ...cellStyle, color: secondaryColor };
+      } else {
+        cellStyle = { ...cellStyle, color: iconColor };
+      }
+
+    }
+
+  return cellStyle;
+};
