c8129adb4756f1b526e2ecb2b3eeb51cc0d78c1d
wukdddang <dnr8874@naver.com>
2023-12-25 23:18:30 +0900

    Feat: MSW2 세팅 완료 - msw@2.0.11, next@14.0.4 - next.js의 호환성 문제로, express 서버를 구동해서 브라우저의 MSW 동작을 모킹하도록 수행 - NEXT_PUBLIC_API_MOCKING이 enabled일 때만 모킹 서버를 동작할 수 있도록 세팅 - 로컬 환경의 ip가 다른 문제로, 임시로 개발자가 수동 세팅을 하도록 구현

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,7 +1,7 @@
-// src/mocks/browser.js
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
+import { setupWorker } from 'msw/browser'
+import { handlers } from './handlers'
 
-const worker = setupWorker(...handlers);
+// This configures a Service Worker with the given request handlers.
+const worker = setupWorker(...handlers)
 
 export default worker;
---
+++ src/mocks/http.ts
@@ -0,0 +1,13 @@
+import { createMiddleware } from '@mswjs/http-middleware';
+import express from 'express';
+import cors from 'cors';
+import { handlers } from './handlers';
+
+const app = express();
+const port = 3333;
+const ip = '192.168.0.2'
+
+app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200, credentials: true }));
+app.use(express.json());
+app.use(createMiddleware(...handlers));
+app.listen(port, ip, () => console.log(`Mock server is running on http://${ip}:${port}`));
